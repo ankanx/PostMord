da29dbe759ee54ed5eb9a3d3b707816d8269646b
Andreas Fransson <Ankanx@gmail.com>
2018-08-12 00:57:06 +0200

    Added Splash Screen, Main Menu, and Level one scenes and scripts.

---
+++ PostMord/Assets/Scrips/SceneManagement/LoadScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public void LoadSCene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
---
+++ PostMord/Assets/Scrips/SlashScreen/Pulsatingtext.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pulsatingtext : MonoBehaviour
+{
+    public bool pulse = false;
+
+    void Update()
+    {
+        if (pulse)
+        {
+            StartCoroutine(FadeTextToFullAlpha(1f, GetComponent<Text>()));
+        }
+        else if (!pulse)
+        {
+            StartCoroutine(FadeTextToZeroAlpha(1f, GetComponent<Text>()));
+        }
+    }
+
+
+
+    public IEnumerator FadeTextToFullAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+        pulse = false;
+    }
+
+    public IEnumerator FadeTextToZeroAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+        pulse = true;
+    }
+    
+}
