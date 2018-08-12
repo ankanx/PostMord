bd2ba5db4bb09f10c1175745278a81b667c96740
Melanie <meltobis@gmail.com>
2018-08-12 20:03:56 +0200

    scoreboard in game and return to menu button

---
+++ PostMord/Assets/Scoreboard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Scoreboard : MonoBehaviour {
+
+    public Save save;
+
+	// Use this for initialization
+	void Start () {
+
+        foreach (List<string> s in save.scoreList.score)
+        {
+
+            gameObject.GetComponent<Text>().text = gameObject.GetComponent<Text>().text + s[0] + ": " + s[1] + "\n";
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
