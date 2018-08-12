31332820a4e67f400b7266e8a97073566bf123cf
Andreas Fransson <Ankanx@gmail.com>
2018-08-12 18:48:03 +0200

    Fixed ingame menu

---
+++ PostMord/Assets/Mainmenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mainmenu : MonoBehaviour {
+
+    public GameObject mainmenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            mainmenu.active = !mainmenu.active;
+            if (Time.timeScale == 1)
+                Time.timeScale = 0;
+            else
+                Time.timeScale = 1;
+        }
+	}
+
+    public void Resume()
+    {
+        mainmenu.active = !mainmenu.active;
+
+            Time.timeScale = 1;
+    }
+}
