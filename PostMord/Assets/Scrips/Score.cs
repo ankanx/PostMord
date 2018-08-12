0fe79bc7bbcfface165e13c58256cf79e9becb2d
Melanie <meltobis@gmail.com>
2018-08-12 02:09:14 +0200

    Score

---
+++ PostMord/Assets/Scrips/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Score : MonoBehaviour {
+
+    public float score;
+    private float elapsedTime = 0;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        score = score + Time.deltaTime * 100;
+
+        //GUI.Label(new Rect(10, 10, 100, 30), "score: " + (int)(score * 100));
+
+        Debug.Log(score);
+ 
+	}
+}
